303cfe9bcb559f700bc416f0ea4daa378fdadfea
william ruiz diaz <edu250485@gmail.com>
2023-06-14 21:21:51 -0300

    antes de iniciar con el css

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const productos=[
+    {
+        id:"tinto-01",
+        titulo:"Tinto 01",
+        imagen:"./img/tintos/01.jpg",
+        categoria:{
+            nombre:"Tintos",
+            id:"tintos"
+        },
+        precio:1000
+    },
+    {
+        id:"tinto-02",
+        titulo:"Tinto 02",
+        imagen:"./img/tintos/02.jpg",
+        categoria:{
+            nombre:"Tintos",
+            id:"tintos"
+        },
+        precio:1000
+    },
+    {
+        id:"tinto-03",
+        titulo:"Tinto 03",
+        imagen:"./img/tintos/03.jpg",
+        categoria:{
+            nombre:"Tintos",
+            id:"tintos"
+        },
+        precio:1000
+    },
+]
